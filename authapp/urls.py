2ef817c9a9fb4105d1558895308076a7e40ad63b
Aleksei <alekseibarannikov@mail.ru>
2021-05-27 02:54:40 +0300

    url changes

--- authapp/urls.py
+++ authapp/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from authapp.views import LoginCreateView, RegisterCreateView, logout, ProfileUpdateView
+from authapp.views import login, RegisterCreateView, logout, ProfileUpdateView
 
 app_name = 'authapp'
 
 urlpatterns = [
-    path('login/', LoginCreateView.as_view(), name='login'),
+    path('login/', login, name='login'),
     path('register/', RegisterCreateView.as_view(), name='register'),
     path('profile/<int:pk>', ProfileUpdateView.as_view(), name='profile'),
     path('logout/', logout, name='logout'),
