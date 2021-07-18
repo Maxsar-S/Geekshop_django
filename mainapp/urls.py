b3e4266c5756f2db1238d38eb597451ea301a7ec
Aleksei <alekseibarannikov@mail.ru>
2021-07-19 00:42:36 +0300

    shablon

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from mainapp.views import products
+from django.views.decorators.cache import cache_page
 
 app_name = 'mainapp'
 
+
+
 urlpatterns = [
     path('', products, name='index'),
-    path('<int:category_id>/', products, name='product'),
+    path('<int:category_id>/', cache_page(3600) (products), name='product'),
     path('page/<int:page>/', products, name='page'),
 ]
